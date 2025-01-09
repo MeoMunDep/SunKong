fbcc208078056b8a9f1968e8ad512a596bd9e59a
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-09 17:05:52 +0700

    this game is scam

--- SunKong/setup.bat
+++ SunKong/setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title name Bot
+title SUNKONG Bot by @MeoMunDep
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    SUNKONG BOT SETUP AND RUN SCRIPT
+echo    SUNKONG BOT SETUP AND RUN SCRIPT by @MeoMunDep
 echo =================================================================
 echo.
 echo Current directory: %CD%
@@ -90,7 +90,7 @@ if exist "..\node_modules" (
 ) else (
     echo Using node_modules from current directory
 )
-node bot
+node MeoMunDep
 pause
 goto MENU
 
